a8d7cb4d2d8d3df5ec662b739f0b917030a9e00f
Abhi527370 <79618928+Abhi527370@users.noreply.github.com>
2024-08-03 19:08:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+     <h1>E-Com Project</h1>
+     <button>Normal Button</button>
+     <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
